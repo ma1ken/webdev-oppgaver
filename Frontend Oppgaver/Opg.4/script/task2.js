7eb40fb396107ae7389ad775885d6365afbc2201
Maiken Edvardsen <maikenjed@hotmail.com>
2024-01-11 17:20:08 +0100

    Finished the whole task with all +/-* and added Number to + bcuz of string. Made own function for numbervalues on other ones.

--- Frontend Oppgaver/Opg.4/script/task2.js
+++ Frontend Oppgaver/Opg.4/script/task2.js
@@ -1,6 +1,5 @@
 // html
 // input
-console.log(showMultiplication);
 
 const inputNumber1 = document.querySelector("#input-number1");
 const inputNumber2 = document.querySelector("#input-number2");
@@ -19,17 +18,59 @@ const outputSubtract = document.querySelector("#output-subtract");
 
 
 // funksjoner
-function showMultiplication() {
+const setNumbers = () => {
     number1 = inputNumber1.value;
-    number2 = inputNumber2.value;
+    number2 = inputNumber2.value;	
+}
+
+// multiply
+const showMultiplication = () => {
+    setNumbers()
     result = multiply ( number1, number2 );
     outputMultiply.innerHTML = result;
 }
 
-function multiply( number1, number2 ) {
+const multiply = ( number1, number2 ) => {
     return number1 * number2;
 }
 
-// buttons
-multiBtn.addEventListener("click", showMultiplication)
+// divide
+const showDividation = () => {
+    setNumbers();
+    result = divide ( number1, number2);
+    outputDivide.innerHTML = result;
+}
 
+const divide = ( number1, number2 ) => {
+    return number1 / number2;
+}
+// add
+const showAdd = () => {
+    number1 = Number (inputNumber1.value);
+    number2 = Number (inputNumber2.value);
+    result = add (number1, number2);
+    outputAdd.innerHTML = result;
+}
+
+const add = (number1, number2) => {
+    return number1 + number2;
+}
+// subtract
+const showSubtract = () => {
+    setNumbers();
+    result = subtract (number1, number2);
+    outputSubtract.innerHTML = result;
+}
+
+const subtract = (number1, number2) => {
+    return number1 - number2;
+}
+
+// buttons
+multiBtn.addEventListener("click", showMultiplication);
+divideBtn.addEventListener("click", showDividation);
+addBtn.addEventListener("click", showAdd);
+subtractBtn.addEventListener("click", showSubtract);
+
+console.log(showMultiplication);
+console.log(showAdd);
